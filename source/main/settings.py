230191783bbcc67cf52ea18b11cb242d1d65bfb7
admin <admin>
2019-12-14 13:57:51 +0600

    done 1 stage without like

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'webapp',
+    'api_v1',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    pictures = models.ImageField(null=True, blank=True,
+                                 upload_to='photo', verbose_name='Картинка')
+    text = models.TextField(null=True, blank=True, max_length=2000, verbose_name='Подпись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    likes = models.IntegerField(default=0, verbose_name='Лайк')
+    author_name = models.ForeignKey(User, on_delete=models.CASCADE,
+                                    null=True, blank=True, max_length=50, verbose_name='Автор фотки')
+
+    def __str__(self):
+        return self.author_name
+
+
+class Comments(models.Model):
+    text = models.TextField(null=True, blank=True, max_length=2000, verbose_name='Текст')
+    photo = models.ForeignKey('webapp.Photo', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Фотография')
+    author_name = models.ForeignKey(User, on_delete=models.CASCADE,
+                                    null=True, blank=True, max_length=50, verbose_name='Автор комента')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+
+class Like(models.Model):
+    pass
